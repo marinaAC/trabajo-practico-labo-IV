1a0a93f52ab0ea581a4f60d11ee876c479ced5ae
marinaAC <maru.cardozo02@gmail.com>
2021-09-02 18:29:07 -0300

    agregado de nuevo favicon y componentes pedidos. sin cambios en la plantilla del tp@

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/pages/login/login.component';
+import { HomeComponent } from './modules/pages/home/home.component';
+import { QuienSoyComponent } from './modules/pages/quien-soy/quien-soy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent
   ],
   imports: [
     BrowserModule
